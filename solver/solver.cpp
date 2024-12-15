9bb202ea9685f02a1a049bbf75b397f03ca2f286
BGY <tyamanakatyamanaka@gmail.com>
2024-12-15 23:14:25 +0900

    recommit

--- solver/solver.cpp
+++ solver/solver.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 class Cell{
     public:
         std::vector<double> points;
+        std::vector<std::vector<double>> jacobian;
+        std::vector<std::vector<double>> element_matrix;
+
+        double gauss_integral(){
+
+    
+            return 0.0;
+        }
+        double calcurate_weak_form_integration_term(){
+
+            return 0.0;
+        }
+
+        void calcurate_jacobian(){
+
+        }
+
+        void make_element_matrix(){
+
+        }
+
+        Cell(std::vector<double> init_points){
+            points = init_points;
+
+            calcurate_jacobian();
+
+            calcurate_weak_form_integration_term();
+
+            make_element_matrix();
+
+        }
 };
+
+
+void main(){
+
+}

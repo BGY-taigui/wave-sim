5bf5b0d42a6fd0254035529e85a588513c05e970
BGY <tyamanakatyamanaka@gmail.com>
2024-12-16 16:57:06 +0900

    coding ShapeFunction class

--- solver/solver.cpp
+++ solver/solver.cpp
@@ -1,35 +1,99 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <armadillo>
+
+using namespace arma;
+
+class ShapeFunction{
+    public:
+        //自然座標系での微分値
+        vec x;
+        vec y;
+        vec z;
+
+        mat jacobian;
+        mat jacobian_inv;
+
+        //直行座標系での微分値
+        vec xi = {
+            -1/8,1/8,1/8,-1/8,-1/8,1/8,1/8,-1/8
+        };
+        vec eta = {
+            -1/8,-1/8,1/8,1/8,-1/8,-1/8,1/8,1/8
+        };
+        vec zeta = {
+            -1/8,-1/8,-1/8,-1/8,1/8,1/8,1/8,1/8,
+        };
+
+
+    ShapeFunction(mat points){
+
+        jacobian = mat(3,3,arma::fill::zeros);
+        make_jacobian(points);
+
+        calcurate_jacobian_inv();
+        calcurate_Nxyz();
+
+    }
+
+    void make_jacobian(mat points){
+
+        jacobian.row(0) = arma::dot(xi,points);
+        jacobian.row(1) = arma::dot(eta,points);
+        jacobian.row(2) = arma::dot(zeta,points);
+
+    }
+
+    void calcurate_jacobian_inv(){
+
+        jacobian_inv = arma::inv(jacobian);
+
+    }
+
+    void calcurate_Nxyz(){
+
+        mat N_xietazeta(3,3);
+        mat Nxyz(3,3);
+
+        N_xietazeta.row(0)=xi;
+        N_xietazeta.row(1)=eta;
+        N_xietazeta.row(2)=zeta;
+
+        Nxyz = arma::dot(jacobian_inv,N_xietazeta);
+
+        x = Nxyz.row(0);
+        y = Nxyz.row(1);
+        z = Nxyz.row(2);
+
+    }
+};
 
 class Cell{
     public:
-        std::vector<double> points;
-        std::vector<std::vector<double>> jacobian;
-        std::vector<std::vector<double>> element_matrix;
+
+        mat points;
+        mat element_matrix;
 
         double gauss_integral(){
 
     
             return 0.0;
         }
+
         double calcurate_weak_form_integration_term(){
 
             return 0.0;
-        }
-
-        void calcurate_jacobian(){
-
         }
 
         void make_element_matrix(){
 
         }
 
-        Cell(std::vector<double> init_points){
+        Cell(mat init_points){
             points = init_points;
 
-            calcurate_jacobian();
+            ShapeFunction N(points);
 
             calcurate_weak_form_integration_term();
 
@@ -39,6 +103,7 @@ class Cell{
 };
 
 
-void main(){
+int main(){
 
+    return 0;
 }
